74ddc2204ab0c2827ee2c56fb238b8c2373b1b0f
Amit K <amitsk1974@gmail.com>
2023-01-21 22:48:16 -0800

    Added record class for testig log

--- src/main/java/com/github/amitsk/todosbatch/functions/TodosCommandLineRunner.java
+++ src/main/java/com/github/amitsk/todosbatch/functions/TodosCommandLineRunner.java
@@ -7,12 +7,14 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 
+record Rectangle(double length, double width) { }
 @Component
 public class TodosCommandLineRunner implements CommandLineRunner, Ordered {
     Logger logger = LoggerFactory.getLogger(TodosCommandLineRunner.class);
     @Override
     public void run(String... args) throws Exception {
-        logger.info(" First command...");
+        var rect = new Rectangle(100.0, 200.0);
+        logger.info(" Printing a rectangle...{}", rect);
     }
 
     @Override
